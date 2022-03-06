bdf4ed6c60e9a96347efdbc1c506ae6c09b754af
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-03-06 16:03:08 +0300

    Added readme

---
+++ ethersWithQuickNode/README.md
@@ -0,0 +1,11 @@
+# The contract
+
+Created via Ethereum Remix IDE
+
+## The Node
+
+To interact with contract, contract must be deployed on blockchain or at a local node
+
+## Web3 Library
+
+Using ethers.js inside node JS application to interact with deployed contract
--- ethersWithQuickNode/price.js
+++ ethersWithQuickNode/price.js
@@ -1,7 +1,7 @@
 var ethers = require('ethers')
-var url = 'ADD_YOUR_ETHEREUM_NODE_URL' // QUICKNODE ETC
+var url = 'HTTP://127.0.0.1:7545' // ADD_YOUR_ETHEREUM_NODE_URL QUICKNODE ETC
 var provider = new ethers.providers.JsonRpcProvider(url)
-var address  =  '0xCB3D1659fB352D24629C1aAdffa0DEe37c78811e' // CONTRACT_ADDRESS_FROM_REMIX
+var address  =  '0x5374d086585AC32009C8d7754a6D732d31a5ED39' // CONTRACT_ADDRESS_FROM_REMIX
 var abi = [
 	{
 		"inputs": [],
@@ -23,6 +23,10 @@ var abi = [
 	}
 ];
 var contract = new ethers.Contract(address,abi,provider)
+
+// Getting the accounts
+const signer0 = provider.getSigner(0);
+const signer1 = provider.getSigner(1);
 
 contract.getLatestPrice().then((result) =>{
   console.log("$" +result.toNumber()/100000000);
