d5e10ef4927d6acb12254e124ee69f850e6044f1
Ismail Erdem Sirma <ismailsirma@gmail.com>
2022-03-26 19:18:13 +0300

    Fixed contracts not working

--- ethersWithHardhatRealContract/hardhat.config.js
+++ ethersWithHardhatRealContract/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   defaultNetwork: "ropsten",
   networks: {
     hardhat: {
-        blockGasLimit: 200000,
+        blockGasLimit: 875000000,
         //gasPrice: 2,
         //gas: 6,
     },
@@ -16,8 +16,8 @@ module.exports = {
       url: 'http://127.0.0.1:8545',
       gasPrice: 1,
       chainId: 31337,
-      blockGasLimit: 200000,
-      gas: 200000,
+      blockGasLimit: 875000000,
+      gas: 875000000,
       accounts: [
         '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
         '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
